0c233081d03e429b890fd146887a29672c02633f
Juan Guillermo Moreno <juang@estudiantes.uci.cu>
2026-03-14 12:53:24 -0500

    Implementacion de la barra lateral

---
+++ App/FrontEnd/just-frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function DashboardPage() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Panel de Control</h1>
+      <p>Bienvenido al panel de control de Distribuidores Just. Aquí puedes gestionar tus productos, inventario, clientes y ventas de manera eficiente.</p>
+    </div>
+  );
+}
--- App/FrontEnd/just-frontend/src/app/page.tsx
+++ App/FrontEnd/just-frontend/src/app/page.tsx
@@ -3,10 +3,8 @@ import { Button } from "@/shared/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="">
       <div>
         <p>esto es un button de la nueva libreria <Button >Click me</Button></p>
       </div>
-    </div>
   );
 }
